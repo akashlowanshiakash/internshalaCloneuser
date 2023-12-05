ca54f4c302659e824c40308ce53a58c2f4e54933
akash lowanshi <akashlowanshi86126@gmail.com>
2023-12-05 20:42:19 +0530

    first commit

---
+++ utils/axios.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+
+const instance = axios.create({
+    // baseURL: "https://internshalabackend-yjc6.onrender.com",
+    baseURL: "http://localhost:8080",
+    withCredentials: true,
+    // headers: {
+    //     'Access-Control-Allow-Origin' : '*',
+    //     'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',   
+    // }
+});
+
+export default instance;
