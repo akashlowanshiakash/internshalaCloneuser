c6ca0278e2d374f3dc35e450f5b022e4ee515296
akash lowanshi <akashlowanshi86126@gmail.com>
2023-12-05 20:47:05 +0530

    local

--- utils/axios.js
+++ utils/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    // baseURL: "https://internshalabackend-yjc6.onrender.com",
-    baseURL: "http://localhost:8080",
+    baseURL: "https://internshalabackend-yjc6.onrender.com",
+    // baseURL: "http://localhost:8080",
     withCredentials: true,
     // headers: {
     //     'Access-Control-Allow-Origin' : '*',
